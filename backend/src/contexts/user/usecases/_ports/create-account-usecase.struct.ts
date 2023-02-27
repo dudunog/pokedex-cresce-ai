5b7bd469c7db998b957232a4b43422fdbca15d74
Eduardo Nogueira <eduardodudunog@gmail.com>
2023-02-27 15:31:46 -0300

    test: testing controllers of user context

--- backend/src/contexts/user/usecases/_ports/create-account-usecase.struct.ts
+++ backend/src/contexts/user/usecases/_ports/create-account-usecase.struct.ts
@@ -1,8 +1,8 @@
-import { AccountModel } from "@contexts/user/domain/models/account-model.struct";
+import { AccountModel, AccountModelWithUserData } from "@contexts/user/domain/models/account-model.struct";
 import { CreationModel } from "@shared/protocols/creation-model";
 import { UseCase } from "@shared/protocols/usecase";
 
-export type ICreateAccountUseCaseDTO = CreationModel<AccountModel>;
+export type ICreateAccountUseCaseDTO = CreationModel<AccountModelWithUserData>;
 
 export type ICreateAccountUseCase = UseCase<
   ICreateAccountUseCaseDTO,
